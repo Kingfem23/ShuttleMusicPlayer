a70e9185cac1a7b29bd351808b83166a7d673e57
Matthew Ajayi <ajayim@uoguelph.ca>
2020-05-31 19:38:54 -0400

    Shuttle Music Player Final

---
+++ app/src/main/java/com/simplecity/amp_library/dagger/component/AppComponent.java
@@ -0,0 +1,25 @@
+package com.simplecity.amp_library.dagger.component;
+
+import com.simplecity.amp_library.dagger.module.*;
+import com.simplecity.amp_library.ui.dialog.DeleteDialog;
+import com.simplecity.amp_library.ui.views.multisheet.CustomMultiSheetView;
+import dagger.Component;
+
+import javax.inject.Singleton;
+
+@Singleton
+@Component(modules = {
+        AppModule.class,
+        ModelsModule.class,
+        DrawerModule.class
+})
+
+public interface AppComponent {
+
+    ActivityComponent plus(ActivityModule module);
+
+    void inject(CustomMultiSheetView target);
+
+    void inject(DeleteDialog target);
+
+}
